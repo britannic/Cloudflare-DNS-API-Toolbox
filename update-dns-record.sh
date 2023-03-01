fbb13b609240ed0c555c81cfbc4f772a98e7c0ae
britannic <britannic@users.noreply.github.com>
2023-03-01 15:46:06 -0800

    Create update-dns-record.sh

---
+++ update-dns-record.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export CF_API_EMAIL=$(cat .cloudflare-id)
+export CF_API_KEY=$(cat .cloudflare-token)
+
+domain=$1
+record=$3.$1
+value=$4
+type=$2
+
+#FIRST GET ZONE IDENTIFIER
+domainid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$1" \
+	-H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" \
+	-H "Content-Type: application/json" \
+	| jq .result[].id -r)
+
+echo $domainid
+     
+curl -s PUT "https://api.cloudflare.com/client/v4/zones/$domainid/dns_records" \
+	-H "X-Auth-Key: $CF_API_KEY" -H "X-Auth-Email: $CF_API_EMAIL" \
+	-H "Content-Type: application/json" \
+	--data '{"type":"'$type'","name":"'$record'","content":"'$value'","ttl":3600,"priority":10,"proxied":false}' \
+	| jq .
+
+#	| jq .success
